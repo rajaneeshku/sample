03e029dd22d092a2a98bbda8e5d9fc7243d4b353
rajaneesh koratamaddi <hi.rajaneesh@otlook.com>
2020-07-26 17:16:11 -0500

    intial commit

---
+++ src/main/java/com/example/learning/LearningDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningDemoApplication.class, args);
+	}
+
+}
